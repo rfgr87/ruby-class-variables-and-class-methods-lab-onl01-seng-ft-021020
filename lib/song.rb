a0148714bf22271ce9dfb365d8e0b49ebab729e0
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-20 16:28:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  attr_accessor :song_name, :artist, :genre
+  @@count = 0
+  @@genre = []
+  
+  def initialize(song_name, artist, genre)
+    @song_name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genre << genre
+  end
+
+  def self.count
+    @@count
+  end
+  
+  def self.genre
+    
+  
+  
+  
+  
+end 
